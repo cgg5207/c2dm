4ed62bed34ae2702ca4b2e9743c04ae5e2f3fa95
Yasith <yasith@favoritemedium.com>
2010-12-21 12:00:31 +0800

    Adding some new methods to facilitate sending of custom key value pairs in C2DM messages.

--- lib/c2dm.rb
+++ lib/c2dm.rb
@@ -31,6 +31,40 @@ module C2DM
       response
     end
 
+    # Construct the html parameter, value string from the give map object
+    def self.get_data_string(map)
+      data = ''
+      key_value_pairs.keys.each do |k|
+        data = "#{data}&data.#{k.to_s}=#{CGI::escape(map[k].to_s)}"
+      end
+      
+      data
+    end
+
+    # Send a C2DM notification with a set of other parameters and values, as given in the map
+    def send_notification_with_kv_map(registration_id, map)
+      post_body = "registration_id=#{registration_id}&collapse_key=foobar&#{Push.get_data_string(map)}"
+      params = {:body => post_body,
+                :headers => {'Authorization' => "GoogleLogin auth=#{@auth_token}"}}
+
+      response = Push.post(PUSH_URL, params)
+      response
+    end
+
+    # Send C2DM notifications with a set of other parameters and values, as given in the map.
+    # The notifications array should consists of map objects like:
+    # {:registration_id => "x", :key_value_pairs => { :key_one => "value_one", :key_two => "value_two" }}
+    # The passed in key value pairs will be sent through C2DM
+    def self.send_notifications_with_kv_map(username, password, source, notifications)
+      c2dm = Push.new(username, password, source)
+    
+      responses = []
+      notifications.each do |notification|
+        responses << {:body => c2dm.send_notification_with_kv_map(notification[:registration_id], notification[:key_value_pairs]), :registration_id => notification[:registration_id]}
+      end
+      responses
+    end
+
     def self.send_notifications(username, password, source, notifications)
       c2dm = Push.new(username, password, source)
     
