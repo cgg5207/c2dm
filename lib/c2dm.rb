57b11e4e009f10e86571e2b2c4ee4f8ea2c9ce8c
Yasith Fernando <thekindofme@gmail.com>
2011-03-04 17:37:49 +0800

    Change the retry count to 4 (from 3)

--- lib/c2dm.rb
+++ lib/c2dm.rb
@@ -43,6 +43,7 @@ module C2DM
       rescue Exception => ex
         if handle_exceptions
           C2DM::C2dmLogger.log.fatal result="Exception in send_notification_wiht_kv_map [exception: #{ex}]"
+          ap ex.backtrace
           result
         else
           raise ex
@@ -138,21 +139,21 @@ module C2DM
       false
     end
 
-    MAX_RETRIES_FOR_TIMEOUT_EX = 3
+    MAX_RETRIES_FOR_TIMEOUT_EX = 4
     # Handle a Timeout Exception, return true if successfully handled, false if not.
     def self.handle_timeout_exception ex, exceptions, counts
       exceptions << ex.to_s
       counts[:timeout_count] = counts[:timeout_count] +1
       counts[:timeout_count_consecative] = counts[:timeout_count_consecative] +1
       C2DM::C2dmLogger.log.warn "Timeout::Error retrying [count:#{counts[:timeout_count_consecative]}, exception:#{ex}]"
-      if counts[:timeout_count_consecative] == MAX_RETRIES_TIMEOUT_EX + 1 # max retries = 3, so break if this is the 4th time
+      if counts[:timeout_count_consecative] == MAX_RETRIES_TIMEOUT_EX + 1 # max retries = X, so break if this is the 4th time
         C2DM::C2dmLogger.log.fatal "FATAL Timeout::Error, giving up [count:#{counts[:timeout_count_consecative]}, exception:#{ex}]"
         return false
       end
       true
     end
 
-    MAX_RETRIES_FOR_QUOTA_EXCEEDED_EX = 3
+    MAX_RETRIES_FOR_QUOTA_EXCEEDED_EX = 4
     # Handle a Quota Exceeded Exception, return true if successfully handled, false if not.
     def self.handle_quota_exceeded_exception ex, exceptions, counts
       exceptions << ex.to_s
@@ -162,7 +163,7 @@ module C2DM
       C2DM::C2dmLogger.log.warn "C2DM::QuotaExceededException retrying after #{QUOTA_EXCEEDED_RETRY_INTERVAL} seconds [count:#{counts[:quota_exceeded_count_consecative]}, exception:#{ex}]"
 
       sleep QUOTA_EXCEEDED_RETRY_INTERVAL
-      if counts[:quota_exceeded_count_consecative] == MAX_RETRIES_FOR_QUOTA_EXCEEDED_EX + 1 # max retries = 3, so break if this is the 4th time
+      if counts[:quota_exceeded_count_consecative] == MAX_RETRIES_FOR_QUOTA_EXCEEDED_EX + 1 # max retries = X, so break if this is the 4th time
         C2DM::C2dmLogger.log.fatal "FATAL C2DM::QuotaExceededException, giving up [count:#{counts[:quota_exceeded_count_consecative]}, exception:#{ex}]"
         return false
       end
