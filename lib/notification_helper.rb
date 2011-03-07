b083a9c6c7a928b4c0b126aeb12f235e14d3b221
Yasith Fernando <thekindofme@gmail.com>
2011-03-07 11:41:22 +0800

    Commenting and refactoring

--- lib/notification_helper.rb
+++ lib/notification_helper.rb
@@ -1,24 +1,24 @@
 require 'httparty'
 require "c2dm_logger.rb"
-require "ap"
 
+# This module contains a number of helper methods used for sending c2dm notifications
 module NotificationHelper
   include HTTParty
   default_timeout 30
 
-  # Construct the html parameter, value string from the give map object
+  # Construct the html parameter, value string from the given map object
   def get_data_string(map)
     data = ''
     map.keys.each do |k|
       data = "#{data}&data.#{k.to_s}=#{CGI::escape(map[k].to_s)}"
     end
-
     data
   end
 
+  # build our own response from the httparty response object
   def parse_push_response httparty_response
     result = {
-        :response => parse_response(httparty_response), # the string ex:- Error=NotRegistered
+        :response => parse_response(httparty_response), # parse_response gives => ex:- Error=NotRegistered
         :http_status_code => httparty_response.response
     }
 
@@ -27,6 +27,8 @@ module NotificationHelper
   end
 
   ERROR_STRING = "Error="
+
+  # Detect a error and extract it if present. If not just get the returned response string
   def parse_response httparty_response
     {
         :is_error => is_error=is_error?(httparty_response),
@@ -38,9 +40,9 @@ module NotificationHelper
     }
   end
 
-  # Check and identify a whether this response contains a error
+  # Check and identify whether this response contains a error
   # make sure the status is 200 and the string does not contain the defined error string
-  def is_error? raw_response
-    raw_response.response.class != Net::HTTPOK || raw_response.parsed_response.include?(ERROR_STRING)
+  def is_error? httparty_response
+    httparty_response.response.class != Net::HTTPOK || httparty_response.parsed_response.include?(ERROR_STRING)
   end
 end
