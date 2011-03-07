bcdfe89d8efb16bb52a27a7d0f911e34d0af64ca
Yasith Fernando <thekindofme@gmail.com>
2011-03-07 11:52:24 +0800

    * Log to a file * Some commenting

--- lib/c2dm_logger.rb
+++ lib/c2dm_logger.rb
@@ -1,19 +1,25 @@
+# Provide some logging to the c2dm lib using the inbuilt 'logger'
 module C2DM
+  # Main class hosting all the logic functionality. Host a single Logger instance and force every logging request
+  # to go through it.
   class C2dmLogger
     require 'logger'
     require "ap"
 
     APP_NAME = "c2dm_lib"
-    LOGGER_INSTANCE = Logger.new(STDOUT)
+    LOGGER_INSTANCE = Logger.new("log/#{APP_NAME}.log", 10, 1024000) # log to a file, limit to 1MB/(rotate) 10 files
+    #LOGGER_INSTANCE = Logger.new(STDOUT) # log to the std out
     LOGGER_INSTANCE.level = Logger::DEBUG
     C2DM_LOGGER_INSTANCE = C2dmLogger.new
 
+    # get the logger instance
     def self.log
       C2DM_LOGGER_INSTANCE
     end
 
+    # redirect all calls to methods, to the logger instance
     def method_missing(m, *args, &block)
-	    LOGGER_INSTANCE.send(m, APP_NAME) {ap(args[0])}
+	    LOGGER_INSTANCE.send(m, APP_NAME) {args[0]}
 	  end
   end
 end
