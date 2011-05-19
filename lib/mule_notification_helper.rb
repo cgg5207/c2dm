8ec0711d87e84f7b8ba8d86fd0e0c4d63fb9acf8
Yasith Fernando <thekindofme@gmail.com>
2011-05-19 16:41:48 +0800

    Cleanup the helper by removing unused methods

--- lib/mule_notification_helper.rb
+++ lib/mule_notification_helper.rb
@@ -28,34 +28,4 @@ module MuleNotificationHelper
     data
   end
 
-  # build our own response from the httparty response object
-  def parse_push_response httparty_response
-    result = {
-        :response => parse_response(httparty_response), # parse_response gives => ex:- Error=NotRegistered
-        :http_status_code => httparty_response.response.code # ie: 200 if sucessful
-    }
-
-    C2DM::C2dmLogger.log.debug "parse_response [#{result}]"
-    result
-  end
-
-  ERROR_STRING = "Error="
-
-  # Detect a error and extract it if present. If not just get the returned response string
-  def parse_response httparty_response
-    {
-        :is_error => is_error=is_error?(httparty_response),
-        :description => if is_error
-                          httparty_response.parsed_response.gsub(ERROR_STRING, "")
-                        else
-                          httparty_response.parsed_response
-                        end
-    }
-  end
-
-  # Check and identify whether this response contains a error
-  # make sure the status is 200 and the string does not contain the defined error string
-  def is_error? httparty_response
-    httparty_response.response.class != Net::HTTPOK || httparty_response.parsed_response.include?(ERROR_STRING)
-  end
 end
